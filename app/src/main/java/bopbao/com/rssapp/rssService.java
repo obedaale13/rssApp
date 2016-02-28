325e402682810d840abbcc4360448a65186073ce
BopBao <kcorionstrong@hotmail.com>
2016-02-27 22:12:41 -0500

    Enter value to service and broadcast it back

--- app/src/main/java/bopbao/com/rssapp/rssService.java
+++ app/src/main/java/bopbao/com/rssapp/rssService.java
@@ -1,34 +1,37 @@
 package bopbao.com.rssapp;
 
 import android.content.Intent;
-import android.app.Service;
+
+import android.app.IntentService;
 import android.os.IBinder;
 import android.os.Binder;
+import android.os.SystemClock;
+import android.util.Log;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 /**
  * Created by Casey on 2016-02-26.
  */
-public class rssService extends Service {
+public class rssService extends IntentService {
 
-    private final IBinder rssBinder = new MyLocalBinder();
+    public static final String passIn = "in message";
+    public static final String passOut = "out message";
 
     public rssService(){
-
+        super("rssService");
     }
 
-    @Override
-    public IBinder onBind(Intent i){
-       return rssBinder;
-    }
+   @Override
+   protected void onHandleIntent(Intent workIntent){
+       String myMessage = workIntent.getStringExtra(passIn);
+       SystemClock.sleep(3000);
+       String result = "This is my messaged passed in: " + myMessage;
 
-    public class MyLocalBinder extends Binder{
-        rssService getService(){
-            return rssService.this;
-        }
-    }
+       Intent broadcastIntent = new Intent();
+       broadcastIntent.addCategory(Intent.CATEGORY_DEFAULT);
+       broadcastIntent.putExtra(passOut, result);
+   }
 
-    public String[] getString (){
-        String[] test = {"Hello", "this", "is", "my", "test"};
-        return test;
-    }
 }
